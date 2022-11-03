9dbe308f8a56595314710a3cb8e806c4bb509561
emilio <emilioblasco@icloud.com>
2022-11-03 09:52:29 +0100

    animacion header

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
-const buttons = document.getElementById("btn");
-const items = document.getElementById("items");
+const buttons = document.getElementById('btn')
+const items = document.getElementById('items')
 
-const btnNav = document.getElementById("toggle");
-const nav = document.getElementById("nav");
+const btnNav = document.getElementById('toggle')
+const nav = document.getElementById('nav')
+
+const header = document.getElementById('header')
+let lastScroll = 0
 
 //****************EVENTOS****************
 
@@ -12,60 +15,76 @@ const nav = document.getElementById("nav");
 //Al desactivar el evento una vez se ejecute
 const onceTime = {
   once: true,
-};
+}
 
 //Movimiento a la izquierda
 
 const moveLeft = () => {
   //Identificamos el primer elemento
-  const firstItem = items.querySelectorAll(".carrusel__item")[0];
-  const width = firstItem.offsetWidth;
+  const firstItem = items.querySelectorAll('.carrusel__item')[0]
+  const width = firstItem.offsetWidth
 
   //Aplicamos la animación
-  items.style.transition = "transform .5s linear";
-  items.style.transform = `translateX(-${width}px)`;
+  items.style.transition = 'transform .5s linear'
+  items.style.transform = `translateX(-${width}px)`
   //Colocamos la primera imagen en última posición
   setTimeout(() => {
-    items.insertAdjacentElement("beforeend", firstItem);
-    items.style.transition = "none";
-    items.style.transform = "translateX(0px)";
+    items.insertAdjacentElement('beforeend', firstItem)
+    items.style.transition = 'none'
+    items.style.transform = 'translateX(0px)'
     // Volvemos a activar el evento, si no sólo se ejecutaría el evento una vez
-    buttons.addEventListener("click", move, onceTime);
-  }, 500);
-};
+    buttons.addEventListener('click', move, onceTime)
+  }, 500)
+}
 
 const moveRight = () => {
   //Identificamos el último elemento
-  const allItems = items.querySelectorAll(".carrusel__item");
-  const lastItem = allItems[allItems.length - 1];
-  const width = lastItem.offsetWidth;
+  const allItems = items.querySelectorAll('.carrusel__item')
+  const lastItem = allItems[allItems.length - 1]
+  const width = lastItem.offsetWidth
 
   //Aplicamos la animación
-  items.style.transition = "transform .5s linear";
-  items.style.transform = `translateX(${width}px)`;
+  items.style.transition = 'transform .5s linear'
+  items.style.transform = `translateX(${width}px)`
   //Colocamos la última imagen en primera posición
   setTimeout(() => {
-    items.insertAdjacentElement("afterbegin", lastItem);
-    items.style.transition = "none";
-    items.style.transform = "translateX(0px)";
+    items.insertAdjacentElement('afterbegin', lastItem)
+    items.style.transition = 'none'
+    items.style.transform = 'translateX(0px)'
     // Volvemos a activar el evento, si no sólo se ejecutaría el evento una vez
-    buttons.addEventListener("click", move, onceTime);
-  }, 500);
-};
+    buttons.addEventListener('click', move, onceTime)
+  }, 500)
+}
 
 function move(e) {
-  if (e.target.id === "btn-left") {
-    moveLeft();
+  if (e.target.id === 'btn-left') {
+    moveLeft()
   }
-  if (e.target.id === "btn-right") {
-    moveRight();
+  if (e.target.id === 'btn-right') {
+    moveRight()
   }
 }
 
 function showNav(e) {
-  nav.classList.toggle("nav__show");
+  nav.classList.toggle('nav__show')
 }
 
-buttons.addEventListener("click", move, onceTime);
+function controlHeader(e) {
+  const currentScroll = window.scrollY
 
-btnNav.addEventListener("click", showNav);
+  if (currentScroll <= 300) {
+    header.classList.remove('oculta-header')
+    header.classList.add('color-transparent')
+  } else if (currentScroll > lastScroll) {
+    header.classList.add('oculta-header')
+  } else if (currentScroll < lastScroll) {
+    header.classList.remove('oculta-header', 'color-transparent')
+  }
+  lastScroll = currentScroll
+}
+
+buttons.addEventListener('click', move, onceTime)
+
+btnNav.addEventListener('click', showNav)
+
+window.addEventListener('scroll', controlHeader)
